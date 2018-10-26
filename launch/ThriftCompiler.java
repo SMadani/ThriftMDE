bf2a54c282aac8aa295b68f4feca0aa55ca4cf50
Sina Madani <sinadoom@googlemail.com>
2018-10-26 14:53:18 +0100

    Updated compiler

--- launch/ThriftCompiler.java
+++ launch/ThriftCompiler.java
@@ -1,17 +1,25 @@
 package org.apache.thrift;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
-import org.apache.thrift.ThriftStandaloneSetup;
+import java.util.Date;
+import java.util.List;
+import org.apache.thrift.IDLStandaloneSetup;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.epsilon.egl.EglFileGeneratingTemplateFactory;
 import org.eclipse.epsilon.egl.EgxModule;
 import org.eclipse.epsilon.egl.execute.context.EgxContext;
+import org.eclipse.epsilon.egl.execute.context.IEgxContext;
 import org.eclipse.epsilon.emc.emf.InMemoryEmfModel;
+import org.eclipse.epsilon.eol.IEolModule;
 import org.eclipse.epsilon.eol.execute.context.FrameStack;
+import org.eclipse.epsilon.eol.execute.context.IEolContext;
 import org.eclipse.epsilon.eol.execute.context.Variable;
+import org.eclipse.epsilon.eol.models.IModel;
 import org.eclipse.epsilon.evl.EvlModule;
+import org.eclipse.epsilon.evl.IEvlModule;
 import org.eclipse.epsilon.evl.execute.UnsatisfiedConstraint;
 import org.eclipse.epsilon.evl.execute.context.IEvlContext;
 import org.eclipse.xtext.resource.XtextResource;
@@ -36,7 +44,7 @@ public class ThriftCompiler {
 		}
 		
 		//Parsing the input file into an EMF model
-		Injector injector = (Injector) new ThriftStandaloneSetup().createInjectorAndDoEMFRegistration();
+		Injector injector = (Injector) new IDLStandaloneSetup().createInjectorAndDoEMFRegistration();
 		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, true);
 		File thriftFile = new File(args[1]);
@@ -44,20 +52,19 @@ public class ThriftCompiler {
 		fileName = fileName.substring(0, fileName.lastIndexOf('.'));
 		String fileLocation = thriftFile.getAbsolutePath();
 		Resource resource = resourceSet.getResource(URI.createURI("file:///"+fileLocation), true);
-		InMemoryEmfModel model = new InMemoryEmfModel(resource);
+		IModel model = new InMemoryEmfModel(resource);
 	    model.load();
 	    
 	    //Semantic validation using Epsilon Validation Language
-	    EvlModule evl = new EvlModule();
+	    IEvlModule evl = new EvlModule();
 	    evl.parse(new File(root+"validation/ThriftValidator.evl"));
 	    if (!evl.getParseProblems().isEmpty()) {
 	    	System.err.println("Syntax errors found in validation logic. Exiting.");
 	    	return;
 	    }
-	    IEvlContext evlContext = evl.getContext();
-	    evlContext.getModelRepository().addModel(model);
+	    evl.getContext().getModelRepository().addModel(model);
 	    evl.execute();
-	    Collection<UnsatisfiedConstraint> failedConstraints = evlContext.getUnsatisfiedConstraints();
+	    Collection<UnsatisfiedConstraint> failedConstraints = evl.getContext().getUnsatisfiedConstraints();
 	    if (!failedConstraints.isEmpty()) {
 	    	System.err.println("Semantic validation failed:");
 	    	failedConstraints.forEach(constraint -> System.out.println(constraint.getMessage()));
@@ -65,16 +72,17 @@ public class ThriftCompiler {
 	    }
 	    
 	    //Code generation using Epsilon Generation Lanaguage
-		EgxModule egx = new EgxModule(new EglFileGeneratingTemplateFactory());
+		IEolModule egx = new EgxModule(new EglFileGeneratingTemplateFactory());
 	    egx.parse(new File(root+"generator/"+args[0]+"/thrift-"+args[0]+".egx"));
 	    if (!egx.getParseProblems().isEmpty()) {
 	    	System.err.println("Syntax errors found in code generation template. Exiting.");
 	    	return;
 	    }
-	    EgxContext egxContext = egx.getContext();
-	    FrameStack egxFrames = egxContext.getFrameStack();
-	    egxContext.getModelRepository().addModel(model);
-	    egxFrames.put(Variable.createReadOnlyVariable("fileName", fileName));
+	    egx.getContext().getModelRepository().addModel(model);
+	    egx.getContext().getFrameStack().put(
+	    	Variable.createReadOnlyVariable("fileName", fileName),
+	    	Variable.createReadOnlyVariable("date", new SimpleDateFormat("yyyy-MM-dd").format(new Date()))
+	    );
 	    egx.execute();
 	}
 }
